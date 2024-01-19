39664e21f2fe3fefe052584cf0c35d5eb33e78c5
DiegoC281 <47009341+DiegoC281@users.noreply.github.com>
2024-01-19 00:34:21 -0300

    Add basic UI

--- js/app.js
+++ js/app.js
@@ -13,53 +13,57 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection == computerSelection) {
-        return playRound(playerSelection, getComputerChoice());
+        return { result: "tie", playerSelection: playerSelection, computerSelection: computerSelection };
     }
     else if (playerSelection == "rock") {
         if (computerSelection == "paper") {
-            return "You Lose! Paper beats Rock";
+            return { result: "lose", loser: playerSelection, winner: computerSelection };;
         }
         else {
-            return "You Win! Rock beats Scissors";
+            return { result: "win", winner: playerSelection, loser: computerSelection };;
         }
     }
     else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
-            return "You Win! Paper beats Rock";
+            return { result: "win", winner: playerSelection, loser: computerSelection };;
         }
         else {
-            return "You Lose! Scissors beat Paper";
+            return { result: "lose", loser: playerSelection, winner: computerSelection };;
         }
     }
     else {
         if (computerSelection == "paper") {
-            return "You Win! Scissors beat Paper";
+            return { result: "win", winner: playerSelection, loser: computerSelection };;
         }
         else {
-            return "You Lose! Rock beats Scissors";
+            return { result: "lose", loser: playerSelection, winner: computerSelection };;
         }
     }
 }
 
-function game() {
-    let computerScore = 0;
-    let playerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt("Please choose your play");
-        let result = playRound(playerChoice, getComputerChoice());
-        console.log(result);
-        let winStatus = result.slice(4, 7);
-        if (winStatus == "Los") {
-            computerScore++;
+const buttons = document.querySelectorAll(".buttons");
+const div = document.querySelector("#result");
+const winner = document.querySelector("#winner");
+let scoreboard = document.querySelector("#scoreboard");
+let playerScore = 0;
+let computerScore = 0;
+
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        let result = playRound(event.target.id, getComputerChoice())
+        if (result.result != "tie") {
+            div.textContent = `You ${result.result}, ${result.winner} beats ${result.loser}.`;
+            result.result == "win" ? playerScore++ : computerScore++;
+            scoreboard.textContent = `Player ${playerScore} - Computer ${computerScore}`;
         } else {
-            playerScore++;
+            div.textContent = "It's a tie, try again";
         }
-    }
-    let winner;
-    if (computerScore > playerScore) {
-        winner = "Computer";
-    } else {
-        winner = "Player";
-    }
-    console.log(`The winner is ${winner}. The score is ${computerScore} - ${playerScore}`);
-}
+        if (playerScore == 5) {
+            winner.textContent = "You won!";
+        } else if (computerScore == 5) {
+            winner.textContent = "You lost!";
+        }
+    });
+
+});
